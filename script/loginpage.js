5b4b9e18bd672aefccfd7c8a60bdd9a30c32a82d
Chanatip Pattanapen <lovels2906@gmail.com>
2023-05-02 00:11:40 +0700

    fix: auth

--- script/loginpage.js
+++ script/loginpage.js
@@ -1,10 +1,8 @@
-// const { authApp } = require("../../ceeproject_backend/controllers");
-
 const backendIPAddress = "127.0.0.1:3000";
 
 const authorizeApplication = () => {
-    window.location.href = `http://${backendIPAddress}/auth_app`;
+  window.location.href = `http://${backendIPAddress}/courseville/auth_app`;
 };
 
-const login_button = document.getElementById('loginbutton');
-login_button.addEventListener('click',authorizeApplication);
+const login_button = document.getElementById("loginbutton");
+login_button.addEventListener("click", authorizeApplication);
